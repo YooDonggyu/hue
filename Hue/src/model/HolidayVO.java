17ab74569f60bd72ce9db72c1b59e1bea43c4615
YUNHEE LEE <yunheenet@gmail.com>
2018-04-03 10:29:49 +0900

    feat: Holiday Model 등록
    HolidayVO, HolidayDAO 초기화 파일 생성

---
+++ Hue/src/model/HolidayVO.java
@@ -0,0 +1,72 @@
+package model;
+
+public class HolidayVO {
+	private int hNo;
+	private String hStartDate;
+	private String hEndDate;
+	private String hRegDate;
+	private String hContent;
+	private String hFlag;
+	private StaffVO staffVO;
+	
+	public HolidayVO(int hNo, String hStartDate, String hEndDate, String hRegDate, String hContent, String hFlag,
+			StaffVO staffVO) {
+		super();
+		this.hNo = hNo;
+		this.hStartDate = hStartDate;
+		this.hEndDate = hEndDate;
+		this.hRegDate = hRegDate;
+		this.hContent = hContent;
+		this.hFlag = hFlag;
+		this.staffVO = staffVO;
+	}
+	
+	public int gethNo() {
+		return hNo;
+	}
+	public void sethNo(int hNo) {
+		this.hNo = hNo;
+	}
+	public String gethStartDate() {
+		return hStartDate;
+	}
+	public void sethStartDate(String hStartDate) {
+		this.hStartDate = hStartDate;
+	}
+	public String gethEndDate() {
+		return hEndDate;
+	}
+	public void sethEndDate(String hEndDate) {
+		this.hEndDate = hEndDate;
+	}
+	public String gethRegDate() {
+		return hRegDate;
+	}
+	public void sethRegDate(String hRegDate) {
+		this.hRegDate = hRegDate;
+	}
+	public String gethContent() {
+		return hContent;
+	}
+	public void sethContent(String hContent) {
+		this.hContent = hContent;
+	}
+	public String gethFlag() {
+		return hFlag;
+	}
+	public void sethFlag(String hFlag) {
+		this.hFlag = hFlag;
+	}
+	public StaffVO getStaffVO() {
+		return staffVO;
+	}
+	public void setStaffVO(StaffVO staffVO) {
+		this.staffVO = staffVO;
+	}
+
+	@Override
+	public String toString() {
+		return "HolidayVO [hNo=" + hNo + ", hStartDate=" + hStartDate + ", hEndDate=" + hEndDate + ", hRegDate="
+				+ hRegDate + ", hContent=" + hContent + ", hFlag=" + hFlag + ", staffVO=" + staffVO + "]";
+	}
+}
